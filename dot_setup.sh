33ccdab283526dd379358b0e1b33e37a2a3ea7a1
naadamki <naadamki@gmail.com>
2025-11-01 19:09:18 -0500

    Update dot_setup.sh

--- dot_setup.sh
+++ dot_setup.sh
@@ -4,7 +4,7 @@ git config --global user.email 'naadamki@gmail.com'
 alias dot='/usr/bin/git --git-dir=$HOME/.dot/ --work-tree=$HOME'
 echo ".dot" >> .gitignore
 git clone --bare https://github.com/naadamki/dotfiles.git $HOME/.dot
-alias config='/usr/bin/git --git-dir=$HOME/.dot/ --work-tree=$HOME'
+alias dot='/usr/bin/git --git-dir=$HOME/.dot/ --work-tree=$HOME'
 
 mkdir -p .config-backup && \
 dot checkout 2>&1 | grep -e "\s+\." | awk {'print $1'} | \
