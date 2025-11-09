cdf3c8da87d5180764bfd0950e8a31954751c6f1
Nathaniel Adam King <naadamki@gmail.com>
2025-11-09 03:27:36 -0600

    updated fish functions

--- .config/fish/functions/cdd.fish
+++ .config/fish/functions/cdd.fish
@@ -1,5 +1,10 @@
 function cdd --description "cd into directory given and subsequent subdirectories." 
-    set -l target_path (string join / -- $argv)
+    if [ (count $argv) -eq 0 ]
+        echo "No directory provided. Changing to home directory (~)."
+        cd ~
+    else
+        set -l target_path (string join / -- $argv)
 
-    builtin cd "$target_path"
+        builtin cd "$target_path"
+    end
 end
